6a3015e10843578e0993f360350985b582632c9b
jpashley1 <jpashley1@gmail.com>
2024-08-29 18:58:57 -0400

    updated with Brian's code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,10 @@
 class PagesController < ApplicationController
 end
+
+def hello_method
+  render json: {message: "hellooooo"}
+end
+
+def large_people
+  render json: {name: "this is a strange route"}
+end
